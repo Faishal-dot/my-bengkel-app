d4679ce6b2036ec4e2435964d9ef509519adff08
Fais Faishal <ninikmasruroh884@gmail.com>
2026-01-14 12:32:34 +0700

    Add customer role to enum

--- database/migrations/2025_10_20_021203_update_role_enum_in_users_table.php
+++ database/migrations/2025_10_20_021203_update_role_enum_in_users_table.php
@@ -6,17 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            // Gunakan standar Laravel '->change()' agar otomatis menyesuaikan database
-            $table->enum('role', ['admin', 'user', 'mechanic'])->default('user')->change();
-        });
-    }
+{
+    Schema::table('users', function (Blueprint $table) {
+        // Tambahkan 'customer' ke dalam array enum
+        $table->enum('role', ['admin', 'user', 'mechanic', 'customer'])->default('user')->change();
+    });
+}
 
     public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'user'])->default('user')->change();
-        });
-    }
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->enum('role', ['admin', 'user', 'mechanic'])->default('user')->change();
+    });
+}
 };
